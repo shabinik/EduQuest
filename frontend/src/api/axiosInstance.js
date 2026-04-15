97a34ecb5c830167e8e6b3b10577c4d653de1f3d
shabin <muhammedshabin2001@gmail.com>
2026-04-15 17:17:31 +0530

    Changed frontend to dynamic axios

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 
-const API_BASE = "http://localhost:8000/api/";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000/api/";
 
 const axiosInstance = axios.create({
     baseURL: API_BASE,
